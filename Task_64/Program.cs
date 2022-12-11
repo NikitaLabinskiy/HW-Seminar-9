e0306bf19937a42695013aa43d4ee0286c8a98bd
Nikita Labinskiy <NLabinskiy@gmail.com>
2022-12-11 21:55:44 +0300

    add gitignore

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. 
+// Напишите метод, который выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+AllNaturalNumbers(n);
+
+void AllNaturalNumbers(int number)
+{
+    int[] array = new int[number];
+    if (number > 0)
+    {
+        Console.Write($"{number--} ");
+        AllNaturalNumbers(number);
+    }
+}
+
+
---
+++ Task_66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите метод, который найдёт сумму натуральных элементов в промежутке от M до N. 
+// Через рекурсию!
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.Write("Введите число m: ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.Write("Введите число n: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+int number = SumOfDigitsBetweenNumbers(m, n);
+
+Console.Write(number);
+
+int SumOfDigitsBetweenNumbers(int number1, int number2, int i = 0)
+{
+    int digit = 0;
+    if (number1 <= number2)
+    {
+        int temp = number1;
+        digit += temp;
+        digit += SumOfDigitsBetweenNumbers(number1 + 1, number2);
+    }
+    return digit;
+}
